d79ad02fdf215ccccea1bdb011c8e30111696d3e
Ryan Hart <3673962+ryankhart@users.noreply.github.com>
2023-03-06 16:34:34 -0800

    Add my/toggle-window-split-direction defun and keybinding

--- config.el
+++ config.el
@@ -450,6 +450,36 @@ insert mode."
   :after (lambda (&rest _)
            (message "Exiting magit-blame mode")))
 
+(defun my/toggle-window-split-direction ()
+  "Toggle the split direction of the current window."
+  (interactive)
+  (if (= (count-windows) 2)
+    (let* ((this-win-buffer (window-buffer))
+            (next-win-buffer (window-buffer (next-window)))
+            (this-win-edges (window-edges (selected-window)))
+            (next-win-edges (window-edges (next-window)))
+            (this-win-2nd (not (and (<= (car this-win-edges)
+                                      (car next-win-edges))
+                                 (<= (cadr this-win-edges)
+                                   (cadr next-win-edges)))))
+            (splitter
+              (if (= (car this-win-edges)
+                    (car (window-edges (next-window))))
+                'split-window-horizontally
+                'split-window-vertically)))
+      (delete-other-windows)
+      (let ((first-win (selected-window)))
+        (funcall splitter)
+        (if this-win-2nd (other-window 1))
+        (set-window-buffer (selected-window) this-win-buffer)
+        (set-window-buffer (next-window) next-win-buffer)
+        (select-window first-win)
+        (if this-win-2nd (other-window 1)))
+      (window-split-toggle))
+    (message "This function only works with two windows.")))
+(map! :leader
+  :desc "Toggle win split direction" "t s" #'my/toggle-window-split-direction)
+
 (use-package! flycheck
   :defer t
   :config
