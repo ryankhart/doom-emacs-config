453e307d9114f63810ee6f6e42031329f9584740
Ryan Hart <3673962+ryankhart@users.noreply.github.com>
2023-03-14 10:49:05 -0700

    Update custom.el

--- custom.el
+++ custom.el
@@ -5,8 +5,9 @@
  ;; If there is more than one, they won't work right.
 )
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-)
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+  '(font-lock-comment-face ((t (:slant italic :foreground "#848d94"))))
+  '(font-lock-doc-face ((t (:slant italic :foreground "#848d94")))))
